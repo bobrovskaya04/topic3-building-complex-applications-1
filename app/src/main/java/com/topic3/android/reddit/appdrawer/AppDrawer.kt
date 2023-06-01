7ef0d31feaad5c3d39efac114ada00bdf00cd8b4
валерия <v.bobrovskaya0207@mail.ru>
2023-06-01 16:55:19 +0300

    Implementing the Application Bar Footer

--- app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
+++ app/src/main/java/com/topic3/android/reddit/appdrawer/AppDrawer.kt
@@ -53,7 +53,54 @@ private fun AppDrawerHeader() {
 
 @Composable
 fun ProfileInfo() {
-  //TODO add your code here
+  ConstraintLayout(
+    modifier = modifier
+      .fillMaxSize()
+      .padding(
+        start = 16.dp,
+        bottom = 16.dp,
+        end = 16.dp
+      )
+  ) {
+    val colors = MaterialTheme.colors
+    val (settingsImage, settingsText, darkModeButton) = createRefs()
+    Icon(
+      modifier = modifier.constrainAs(settingsImage){
+        start.linkTo(parent.start)
+        bottom.linkTo(parent.bottom)
+      },
+      imageVector = Icons.Default.Settings,
+      contentDescription = stringResource(
+        id = R.string.settings
+      ),
+      tint = colors.primaryVariant
+    )
+    Text(
+      fontSize = 10.sp,
+      text = stringResource(R.string.settings),
+      style = MaterialTheme.typography.body2,
+      color = colors.primaryVariant,
+      modifier = modifier
+        .padding(start = 16.dp)
+        .constrainAs(settingsText) {
+          start.linkTo(settingsImage.end)
+          centerVerticallyTo(settingsImage)
+        }
+
+    )
+    Icon(
+      imageVector = ImageVector.vectorResource(id = R.drawable.ic_moon),
+      contentDescription = stringResource(id = R.string.change_theme),
+      modifier = modifier
+        .clickable(onClick = { changeTheme() })
+        .constrainAs(darkModeButton){
+          end.linkTo(parent.end)
+          bottom.linkTo(settingsImage.bottom)
+        },
+      tint = colors.primaryVariant
+    )
+
+  }
 }
 
 @Composable
